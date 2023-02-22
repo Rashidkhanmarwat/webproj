4a7b96564e3d8d276d18e2db151ff4e591f38377
Rashid Nawaz <rashidmarwat.80@gmail.com>
2023-02-22 15:25:14 +0500

    commit

--- student/urls.py
+++ student/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # path("student", views.student, name = 'student'),
     path("", home),
     path("home/", home),
-    path("stdadd/",stdadd),
-    
+    path("stdadd/",stdadd),   
 ]
 
